a8c8984fff9ff82b0460da0327ab72adf37fbfcd
Foxy <53222754+AcridFak3@users.noreply.github.com>
2025-02-05 01:52:48 +0000

    novo

---
+++ src/Repository/TicketRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Ticket;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Ticket>
+ */
+class TicketRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ticket::class);
+    }
+
+    public function findById($userId): array
+    {
+        return $this->createQueryBuilder('t')
+            ->andWhere('t.userId = :userId')  // Agora é a relação de objetos
+            ->setParameter('userId', $userId)  // Definindo o ID do usuário
+            ->orderBy('t.id', 'ASC')  // Ordenação opcional, se precisar
+            ->getQuery()
+            ->getResult();
+    }
+    //    /**
+    //     * @return Ticket[] Returns an array of Ticket objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('t.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Ticket
+    //    {
+    //        return $this->createQueryBuilder('t')
+    //            ->andWhere('t.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
